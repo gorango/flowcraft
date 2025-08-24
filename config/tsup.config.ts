e1f17cc6617cbef3e5af9b44971235ba559d8418
Goran <gospaso@gmail.com>
2025-08-24 09:27:12 +0200

    refactor build to support deep imports and tree-shaking
    Updated the tsup build process to output individual files and shared
    chunks instead of a single bundle. This allows consumers to import only
    the code they need, leading to smaller final application bundles.

--- config/tsup.config.ts
+++ config/tsup.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-	entry: ['src/index.ts'],
+	entry: [
+		'src/**/*.ts',
+		'!src/**/*.test.ts',
+		'!src/test-utils/**/*.ts',
+	],
+	entryPoints: ['src/index.ts'],
 	format: ['esm'],
+	target: 'esnext',
 	dts: true,
-	splitting: false,
-	sourcemap: true,
 	clean: true,
-	minify: true,
+	sourcemap: true,
+	splitting: true,
+	treeshake: true,
+	minify: false,
 })
