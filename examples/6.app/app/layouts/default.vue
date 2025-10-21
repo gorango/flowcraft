2c88b9769b36afb2bffc8b209fc798fdc259b70a
Goran <gospaso@gmail.com>
2025-10-21 08:57:25 +0200

    add header

--- examples/6.app/app/layouts/default.vue
+++ examples/6.app/app/layouts/default.vue
@@ -1,14 +1,36 @@
 <script setup lang="ts">
 const { toggleColorMode } = useTheme()
+
+const pages = [
+	{ name: 'Basic', path: '/basic' },
+	{ name: 'Translate', path: '/translate' },
+	{ name: 'Research', path: '/research' },
+	{ name: 'Declarative', path: '/declarative' },
+]
 </script>
 
 <template>
-	<Button
-		class="fixed top-4 right-4 z-50"
-		size="icon"
-		@click="toggleColorMode"
-	>
-		<Icon name="lucide:moon" />
-	</Button>
+	<header class="border-b">
+		<nav class="max-w-6xl w-full mx-auto p-4 flex items-center gap-2">
+			<Button
+				v-for="page in pages"
+				:key="page.path"
+				variant="ghost"
+				as-child
+			>
+				<NuxtLink :to="page.path">
+					{{ page.name }}
+				</NuxtLink>
+			</Button>
+			<span class="flex-auto" />
+			<Button
+				size="icon"
+				variant="ghost"
+				@click="toggleColorMode"
+			>
+				<Icon name="lucide:moon" />
+			</Button>
+		</nav>
+	</header>
 	<slot />
 </template>
